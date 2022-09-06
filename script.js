a72fb62d99f8a8efdfba22c5579c50f3f640233a
Jenn Guneratne <jenn.guneratne@gmail.com>
2022-09-06 19:27:12 -0400

    Update Pseudocode: Real-world play vs. Console play

--- script.js
+++ script.js
@@ -1,12 +1,38 @@
 // Write a console program for Rock-Paper-Scissors Game
-// Game has two players
-// Players say "Rock, Paper, Scissors, Shoot" and throw their selection on "Shoot"
-// Each player simultaneously plays a Rock, Paper or Scissors
-    // The dominant selection wins the game: 
-        // Rock beats Scissors
-        // Paper beats Rock
-        // Scissors beats Paper
-        // Two of the same selection ends in a draw
-// Declare winner, or Draw
-// Option to play again
+
+
+// Real-World Game Play: 
+    // Players say "Rock, Paper, Scissors, Shoot" and throw their selection on "Shoot"
+    // Each player simultaneously plays a Rock, Paper or Scissors
+        // The dominant selection wins the game: 
+            // Rock beats Scissors
+            // Paper beats Rock
+            // Scissors beats Paper
+            // Two of the same selection ends in a draw
+    // Declare winner, or Draw
+    // Option to play again
+
+
+// Console Game Play: 
+    // Game has two players - one User against the Computer
+
+    // Write a function called getComputerChoice that randomly returns 'Rock', 'Paper' or 'Scissors'
+
+    // Write a function called playRound that plays a single round of Rock-Paper-Scissors
+        // Function takes two parameters: playerSelection and computerSelection
+        // Make sure player selection of parameter is case-insensitive (can take rock, ROCK, RocK, etc)
+        // After taking selections, returns a string that declares the winner of the round: "You Lose! Paper beats Rock"
+        // This function *returns* the results, rather than using console.log
+    
+    // Write a function called game(), and call the playRound() function inside of this one
+        // The Function should play 5 rounds of the game and keeps score (use loops)
+        // Use prompt() to get input from the user
+        // The winner or loser is reported at the end of the 5 rounds
+        // Use console.log() to display the results of each round and the winner at the end
+
+        // Other notes: 
+            // Feel free to rework previous functions if needed
+            // You might want to change the return value to something more useful   
+
+
 
