18b3eb2f757e322075f057f594c3f07f7ab04723
Jenn Guneratne <jenn.guneratne@gmail.com>
2022-09-06 19:10:27 -0400

    Setup for Rock-Paper-Scissors Game

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # rock-paper-scissors
+
+This is TOP's Rock Paper Scissors javascript exercise
+
+I will begin by playing the game in the browser console. 
+
+A GUI will be added at a later date. 
---
+++ script.js
@@ -0,0 +1,14 @@
+console.log("Hello World!"); 
+
+// Write a console program for Rock-Paper-Scissors Game
+// Game has two players
+// Players say "Rock, Paper, Scissors, Shoot" and throw their selection on "Shoot"
+// Each player simultaneously plays a Rock, Paper or Scissors
+    // The dominant selection wins the game: 
+        // Rock beats Scissors
+        // Paper beats Rock
+        // Scissors beats Paper
+        // Two of the same selection ends in a draw
+// Declare winner, or Draw
+// Option to play again
+
