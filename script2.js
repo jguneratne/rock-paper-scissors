50e7a5fb5d814bb2b8ea19d4feebda7d2e00754b
Jenn Guneratne <jenn.guneratne@gmail.com>
2022-10-06 07:36:05 -0400

    Clearnup playRound and game functions

--- script2.js
+++ script2.js
@@ -18,63 +18,26 @@ conditional statements comparing entries and incrementing scores
 
 
 function playRound(playerSelection, computerSelection) { 
-    
-    playerSelection = prompt("Ready to Play? Enter Rock, Paper or Scissors!", "Make your choice here.");
-    computerSelection = getComputerChoice();
-
-    playerSelection = playerSelection.toLowerCase();
-    computerSelection = computerSelection.toLowerCase();
-
-    playerScore = 0;
-    computerScore = 0;
-    tieScore = 0;
-    
+        
     if 
+    (playerSelection === computerSelection) {
+        return `This round was a tie! You both chose ${playerSelection}.`
+
+    } else if 
         (playerSelection === "rock" && computerSelection === "scissors" ||
         playerSelection === "paper" && computerSelection === "rock" ||
         playerSelection === "scissors" && computerSelection === "paper") {
+    
+            return `You win this round! You ${playerSelection} beats their ${computerSelection}.`
 
-        return `You win this round! You chose ${playerSelection}, they chose ${computerSelection}.`
-
-    } else if 
-
-        (playerSelection === "rock" && computerSelection === "paper" ||
-        playerSelection === "paper" && computerSelection === "scissors" ||
-        playerSelection === "scissors" && computerSelection === "rock") {
-            computerScore++;
-        return `You loose this round! You chose ${playerSelection}, they chose ${computerSelection}.`
-
-    } else if 
-            
-        (playerSelection === computerSelection) {
-            
-            return `This round was a tie! You both chose ${playerSelection}.`
     } else {
-
-            return "There was a problem."
+            return `You loose this round! Their ${computerSelection} beats your ${playerSelection}.`
     }
+
 }
 
 //  console.log(playRound()); 
 
-
-/* Build keepScore function
-    Takes score from playRound and adds it for each player
-    Displays score at end of game()
-*/
-
-function keepScore () {
-    playRound(playerScore, computerScore, tieScore);
-
-    for (i = 0; i > 5; i++) {
-        if (playerSelection > computerSelection) {
-            playerScore++;
-        } else {
-            computerScore++
-        } 
-    }
-    
-}
 
 /* Build function that plays 5 rounds of game
 let round = number
@@ -84,14 +47,15 @@ announce winner at the end
 */
 
 function game() {
+    for (let i = 0; i < 5; i++) {
+        playerSelection = prompt("Ready to Play? Enter Rock, Paper or Scissors!", "Make your choice here.");
+        computerSelection = getComputerChoice();
 
-round = 0;
+        playerSelection = playerSelection.toLowerCase();
+        computerSelection = computerSelection.toLowerCase();
 
-do {
-    console.log(playRound(round++));
-    //console.log(keepScore());
-} while (round < 5)
-
-
+        console.log(playRound(playerSelection, computerSelection));
+    }
 }
+
   console.log(game());
